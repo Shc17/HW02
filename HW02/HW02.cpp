205c03c8faa21fd1b228813c9ad346c06fe41aab
Hyeongcheol <gudcjf6543@gmail.com>
2025-06-02 18:03:14 +0900

    Update HW02.cpp

--- HW02/HW02.cpp
+++ HW02/HW02.cpp
@@ -1,3 +1,65 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+// 기본 클래스
+class Animal 
+{
+public:
+    virtual void makesound() = 0; // 순수 가상 함수
+    
+    virtual ~Animal() {} // 소멸자
+};
+
+// 파생 클래스
+class dog : public Animal 
+{
+public:
+    void makesound() 
+    {
+        cout << "Dog: Woof! Woof!" << endl;
+    }
+};
+ // 파생 클래스
+class cat : public Animal 
+{
+public:
+    void makesound() 
+    {
+        cout << "Cat: Meow! Meow!" << endl;
+    }
+};
+ // 파생 클래스
+class cow : public Animal 
+{
+public:
+    void makesound() 
+    {
+        cout << "cow: ummaaa~~" << endl;
+    }
+};
+
+int main() 
+{
+    
+    // 다형성 사용
+    vector<Animal*> animal;
+    animal.push_back(new dog());
+    animal.push_back(new cat());
+    animal.push_back(new cow());
+    
+    
+    // 각 동물의 울음소리
+    for (size_t i = 0; i < animal.size(); ++i) 
+    {
+        animal[i]->makesound();
+    }
+
+    // 메모리 해제
+    for (size_t i = 0; i < animal.size(); ++i) 
+    {
+        delete animal[i];
+    }
+
+    return 0;
+}
